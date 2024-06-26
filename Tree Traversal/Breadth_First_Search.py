dd16fd72d5cd2594c09370a4c6827765437c5c50
bennychristiyan <116448476+bennychristiyan@users.noreply.github.com>
2024-06-26 20:22:50 +0530

    BFS Traversal

---
+++ Tree Traversal/Breadth_First_Search.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while(True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def BFS(self):
+        currentnode = self.root
+        queue = []
+        result = []
+        queue.append(currentnode)
+        while len(queue) > 0:
+            currentnode = queue.pop(0)
+            result.append(currentnode.value)
+            if currentnode.left is not None:
+                queue.append(currentnode.left)
+            if currentnode.right is not None:
+                queue.append(currentnode.right)
+        return result
+
+my_tree = BinarySearchTree()
+
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print(my_tree.BFS())
